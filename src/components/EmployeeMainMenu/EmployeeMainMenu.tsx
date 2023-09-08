144a5cfbab3b31e4e7b705a775bbe7b0e35e87b8
RomanAlxndrvch <heyekzo@gmail.com>
2023-09-07 19:56:30 -0600

    Remove unused imports

--- src/components/EmployeeMainMenu/EmployeeMainMenu.tsx
+++ src/components/EmployeeMainMenu/EmployeeMainMenu.tsx
@@ -1,35 +1,34 @@
-import React, {memo, useCallback, useEffect, useState} from "react";
-import {Route, Routes, useNavigate} from "react-router-dom";
-import {LoginPassword} from "components/LoginPassword/LoginPassword";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "app/store";
+import React, { memo } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { LoginPassword } from 'components/LoginPassword/LoginPassword';
+import { useSelector } from 'react-redux';
 
-import {Menu} from "components/Menu/Menu";
-import {EmployeeRespondType, EmployeeType, IFormInput} from "utils/types";
-import {useAppDispatch} from "hooks/useAppDispatch";
-import {appActions} from "app/app-reducer";
-import {employeesActions} from "components/EmployeesList/employees-reducer";
-
+import { Menu } from 'components/Menu/Menu';
+import { EmployeeType } from 'utils/types';
+import { useAppDispatch } from 'hooks/useAppDispatch';
+import { AppRootStateType } from 'app/store';
 
 export const EmployeeMainMenu = memo(function () {
-        console.log('MainMenu')
+  console.log('MainMenu');
 
-        const navigate = useNavigate()
-        const dispatch = useAppDispatch()
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-        //Data from state
-        const selectedEmployee = useSelector<AppRootStateType, EmployeeType>((state) => state.employees.selectedEmployee)
-        console.log(selectedEmployee)
+  //Data from state
+  const selectedEmployee = useSelector<AppRootStateType, EmployeeType>(
+    (state) => state.employees.selectedEmployee
+  );
+  console.log(selectedEmployee);
 
-
-        return (
-            <div>
-                <Routes>
-                    <Route path={'/employee-menu'} element={<Menu/>}/>
-                    <Route path={'/employee-password'} element={<LoginPassword onSubmit={() => {
-                    }}/>}/>
-                </Routes>
-            </div>
-        )
-    }
-)
+  return (
+    <div>
+      <Routes>
+        <Route path={'/employee-menu'} element={<Menu />} />
+        <Route
+          path={'/employee-password'}
+          element={<LoginPassword onSubmit={() => {}} />}
+        />
+      </Routes>
+    </div>
+  );
+});
